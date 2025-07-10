b8cd21b41e938c26375a92ee572d58ded4c2c4be
Rabin <rabin.awale09@gmail.com>
2025-07-10 21:31:15 +0545

    Features: Add modal for creating and editing categories with form validation

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ToastContainer } from 'react-toastify';
+import '@mantine/core/styles.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
